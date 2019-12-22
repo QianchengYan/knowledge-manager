ee4fe0147da9ba5e29a591cd026688100234993d
yan0201 <1103170268@qq.com>
2019-12-22 15:16:52 +0800

    生产环境配置文件 config 定义，感觉就是把字符串dev 改成了 prod 而且这两个字符串别的地方都没用到

--- knowledge_manager/lib/main.dart
+++ knowledge_manager/lib/main.dart
@@ -31,27 +31,3 @@ void main() {
     },
   );
 }
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: new ThemeData(
-//         // This is the theme of your application.
-
-//         primarySwatch: Colors.blue,
-//       ),
-//       // 由于HomePage的route是‘/’根路由，所以可以不用写home
-//       // home: new HomePage(),
-//       routes: {
-//         HomePage.routeName: (context) {
-//           return HomePage();
-//         },
-//       },
-//     );
-//   }
-// }
